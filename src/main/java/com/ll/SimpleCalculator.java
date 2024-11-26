4ef4f7a772e096973f8e929f35c5cca15137a9bf
paek1127 <paek1127@gmail.com>
2024-11-26 21:55:42 +0900

    assertThat 변경

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -2,6 +2,6 @@ package com.ll;
 
 public class SimpleCalculator {
     public int plus(int num1, int num2) {
-        return 3;
+        return num1 + num2;
     }
 }
--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -3,15 +3,33 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleCalculatorTest {
     @Test
     @DisplayName("1 + 2 = 3")
-    public void testPlus() {
+    public void t1() {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         int rs = simpleCalculator.plus(1, 2);
 
-        assertEquals(3,rs);
+        assertThat(rs).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("2 + 10 = 12")
+    public void t2() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(2, 10);
+
+        assertThat(rs).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("20 + 10 = 30")
+    public void t3() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(20, 10);
+
+        assertThat(rs).isEqualTo(30);
     }
 }
