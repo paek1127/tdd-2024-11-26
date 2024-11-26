c8e2b5163eee06a11c44c8c1d99aef4b95b05677
paek1127 <paek1127@gmail.com>
2024-11-26 22:01:13 +0900

    연산기호 추가

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -4,4 +4,20 @@ public class SimpleCalculator {
     public int plus(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public int reminder(int num1, int num2) {
+        return num1 % num2;
+    }
 }
--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -32,4 +32,40 @@ public class SimpleCalculatorTest {
 
         assertThat(rs).isEqualTo(30);
     }
+
+    @Test
+    @DisplayName("20 - 10 = 10")
+    public void t4() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.minus(20, 10);
+
+        assertThat(rs).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("3 * 2 = 6")
+    public void t5() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.multiply(3, 2);
+
+        assertThat(rs).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("6 / 2 = 3")
+    public void t6() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.divide(6, 2);
+
+        assertThat(rs).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("8 % 4 = 0")
+    public void t7() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.reminder(8, 4);
+
+        assertThat(rs).isEqualTo(0);
+    }
 }
